b22a39578a1f480215d145358c545ba6cd581663
Hunzala Qamar <69155974+hunzalaqamar@users.noreply.github.com>
2021-11-04 01:08:13 +0500

    Update LandingPage.js

--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import companyLogo from "../public/assests/companylogo.png";
+import companyLogo from "../public/assests/companyLogo.png";
 import Header from "../components/Header/Header";
 import { companyWebsiteSrc } from "../constants/companyWebsite";
 import ItemCard from "../components/ItemCard/ItemCard";
