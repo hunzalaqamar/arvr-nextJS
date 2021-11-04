3629daa8a4a35cbd79b7842d1abb4e4f05125b6f
Hunzala <hunzla.qamar@gmail.com>
2021-11-04 14:53:48 +0500

    revised css in landingpage

--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -18,7 +18,7 @@ function LandingPage() {
           isViewLarge={false}
         />
       </div>
-      <Row xs={1} md={2} lg={3} xl={4}>
+      <Row xs={1} md={2} lg={3} xl={4} className="w-100">
         {objs.map((objname) => (
           <Col key={objname}>
             <ItemCard GLBName={objname} />
